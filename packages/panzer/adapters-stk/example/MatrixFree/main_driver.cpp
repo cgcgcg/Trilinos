38c1cd940bb825a4e5f46d74213c4002a2daf312
Christian Glusa <caglusa@sandia.gov>
2023-03-28 12:39:37 -0600

    Panzer: Add example driver for matrix-free

---
+++ packages/panzer/adapters-stk/example/MatrixFree/main_driver.cpp
@@ -0,0 +1,383 @@
+// @HEADER
+// ***********************************************************************
+//
+//           Panzer: A partial differential equation assembly
+//       engine for strongly coupled complex multiphysics systems
+//                 Copyright (2011) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact Roger P. Pawlowski (rppawlo@sandia.gov) and
+// Eric C. Cyr (eccyr@sandia.gov)
+// ***********************************************************************
+// @HEADER
+
+#include "Teuchos_ConfigDefs.hpp"
+#include "Teuchos_RCP.hpp"
+#include "Teuchos_TimeMonitor.hpp"
+#include "Teuchos_DefaultComm.hpp"
+#include "Teuchos_CommHelpers.hpp"
+#include "Teuchos_GlobalMPISession.hpp"
+#include "Teuchos_CommandLineProcessor.hpp"
+#include "Teuchos_XMLParameterListHelpers.hpp"
+#include "Teuchos_FancyOStream.hpp"
+#include "Teuchos_oblackholestream.hpp"
+#include "Teuchos_Assert.hpp"
+#include "Teuchos_as.hpp"
+#include "Teuchos_StackedTimer.hpp"
+
+#include "Panzer_NodeType.hpp"
+
+#include "PanzerAdaptersSTK_config.hpp"
+#include "Panzer_ClosureModel_Factory_TemplateManager.hpp"
+#include "Panzer_PauseToAttach.hpp"
+// #include "Panzer_ResponseLibrary.hpp"
+#include "Panzer_String_Utilities.hpp"
+// #include "Panzer_EpetraLinearObjContainer.hpp"
+// #include "Panzer_BlockedEpetraLinearObjFactory.hpp"
+#include "Panzer_TpetraLinearObjFactory.hpp"
+#include "Panzer_ElementBlockIdToPhysicsIdMap.hpp"
+#include "Panzer_DOFManagerFactory.hpp"
+#include "Panzer_ModelEvaluator.hpp"
+
+#include "Panzer_STK_SquareQuadMeshFactory.hpp"
+#include "Panzer_STK_SetupLOWSFactory.hpp"
+#include "Panzer_STK_WorksetFactory.hpp"
+#include "Panzer_STKConnManager.hpp"
+#include "Panzer_STK_IOClosureModel_Factory_TemplateBuilder.hpp"
+// #include "Panzer_STK_ResponseEvaluatorFactory_SolutionWriter.hpp"
+
+#include "Tpetra_CrsMatrix.hpp"
+#include "MatrixMarket_Tpetra.hpp"
+
+#include "user_app_ClosureModel_Factory_TemplateBuilder.hpp"
+#include "user_app_EquationSetFactory.hpp"
+#include "user_app_BCStrategy_Factory.hpp"
+
+#include <Ioss_SerializeIO.h>
+
+#include <string>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+  typedef panzer::ModelEvaluator<double> PME;
+
+  using Teuchos::RCP;
+  using Teuchos::rcp;
+  using Teuchos::rcp_dynamic_cast;
+
+  int status = 0;
+
+  Teuchos::oblackholestream blackhole;
+  Teuchos::GlobalMPISession mpiSession(&argc, &argv, &blackhole);
+  Kokkos::initialize(argc,argv);
+
+  Teuchos::RCP<Teuchos::FancyOStream> out = Teuchos::rcp(new Teuchos::FancyOStream(Teuchos::rcp(&std::cout,false)));
+  Teuchos::RCP<Teuchos::FancyOStream> pout = Teuchos::rcp(new Teuchos::FancyOStream(Teuchos::rcp(&std::cout,false)));
+  if (mpiSession.getNProc() > 1) {
+    out->setShowProcRank(true);
+    out->setOutputToRootOnly(0);
+  }
+
+  Teuchos::RCP<Teuchos::StackedTimer> stacked_timer;
+  stacked_timer = rcp(new Teuchos::StackedTimer("Total"));
+  Teuchos::RCP<Teuchos::FancyOStream> verbose_out = Teuchos::rcp(new Teuchos::FancyOStream(Teuchos::rcpFromRef(std::cout)));
+  verbose_out->setShowProcRank(true);
+  stacked_timer->setVerboseOstream(verbose_out);
+  Teuchos::TimeMonitor::setStackedTimer(stacked_timer);
+
+  Teuchos::RCP<const Teuchos::MpiComm<int> > comm
+        = rcp_dynamic_cast<const Teuchos::MpiComm<int> >(Teuchos::DefaultComm<int>::getComm());
+
+  try {
+
+    Teuchos::RCP<Teuchos::Time> total_time =
+      Teuchos::TimeMonitor::getNewTimer("User App: Total Time");
+
+    Teuchos::TimeMonitor timer(*total_time);
+
+
+
+    // Parse the command line arguments
+    std::string input_file_name = "user_app.xml";
+    {
+      Teuchos::CommandLineProcessor clp;
+
+      clp.setOption("i", &input_file_name, "User_App input xml filename");
+
+      Teuchos::CommandLineProcessor::EParseCommandLineReturn parse_return =
+         clp.parse(argc,argv,&std::cerr);
+
+      TEUCHOS_TEST_FOR_EXCEPTION(parse_return != Teuchos::CommandLineProcessor::PARSE_SUCCESSFUL,
+                            std::runtime_error, "Failed to parse command line!");
+    }
+
+    // Parse the input file and broadcast to other processes
+    Teuchos::RCP<Teuchos::ParameterList> input_params = Teuchos::rcp(new Teuchos::ParameterList("User_App Parameters"));
+    Teuchos::updateParametersFromXmlFileAndBroadcast(input_file_name, input_params.ptr(), *comm);
+
+    RCP<Teuchos::ParameterList> mesh_pl             = rcp(new Teuchos::ParameterList(input_params->sublist("Mesh")));
+    RCP<Teuchos::ParameterList> physics_blocks_pl   = rcp(new Teuchos::ParameterList(input_params->sublist("Physics Blocks")));
+    RCP<Teuchos::ParameterList> lin_solver_pl       = rcp(new Teuchos::ParameterList(input_params->sublist("Linear Solver")));
+    Teuchos::ParameterList & block_to_physics_pl    = input_params->sublist("Block ID to Physics ID Mapping");
+    Teuchos::ParameterList & bcs_pl                 = input_params->sublist("Boundary Conditions");
+    Teuchos::ParameterList & closure_models_pl      = input_params->sublist("Closure Models");
+    Teuchos::ParameterList & user_data_pl           = input_params->sublist("User Data");
+
+    user_data_pl.set<RCP<const Teuchos::Comm<int> > >("Comm", comm);
+
+    RCP<panzer::GlobalData> globalData = panzer::createGlobalData();
+    RCP<user_app::MyFactory> eqset_factory = Teuchos::rcp(new user_app::MyFactory);
+    user_app::BCFactory bc_factory;
+
+    user_app::MyModelFactory_TemplateBuilder cm_builder;
+    panzer::ClosureModelFactory_TemplateManager<panzer::Traits> cm_factory;
+    cm_factory.buildObjects(cm_builder);
+
+    // read in mesh database, build un committed data
+    ////////////////////////////////////////////////////////////////
+    RCP<panzer_stk::STK_MeshFactory> mesh_factory = rcp(new panzer_stk::SquareQuadMeshFactory);
+    mesh_factory->setParameterList(mesh_pl);
+
+    RCP<panzer_stk::STK_Interface> mesh = mesh_factory->buildUncommitedMesh(MPI_COMM_WORLD);
+
+    // read in physics blocks
+    ////////////////////////////////////////////////////////////
+
+    std::map<std::string,std::string> block_ids_to_physics_ids;
+    panzer::buildBlockIdToPhysicsIdMap(block_ids_to_physics_ids, block_to_physics_pl);
+
+    std::map<std::string,Teuchos::RCP<const shards::CellTopology> > block_ids_to_cell_topo;
+    for(auto itr=block_ids_to_physics_ids.begin();itr!=block_ids_to_physics_ids.end();itr++)
+      block_ids_to_cell_topo[itr->first] = mesh->getCellTopology(itr->first);
+
+    std::vector<Teuchos::RCP<panzer::PhysicsBlock> > physicsBlocks;
+
+    // setup some defaults
+    int workset_size = 20;
+    int default_integration_order = 2;
+    bool build_transient_support = false;
+    std::vector<std::string> tangentParamNames;
+
+    panzer::buildPhysicsBlocks(block_ids_to_physics_ids,
+                               block_ids_to_cell_topo,
+                               physics_blocks_pl,
+                               default_integration_order,
+                               workset_size,
+                               eqset_factory,
+                               globalData,
+                               build_transient_support,
+                               physicsBlocks,
+                               tangentParamNames);
+
+   // Add fields to the mesh data base (this is a peculiarity of how STK classic requires the
+   // fields to be setup)
+   //////////////////////////////////////////////////////////////////////////////////////////
+   for(std::size_t i=0;i<physicsBlocks.size();i++) {
+      RCP<panzer::PhysicsBlock> pb = physicsBlocks[i]; // we are assuming only one physics block
+
+      const std::vector<panzer::StrPureBasisPair> & blockFields = pb->getProvidedDOFs();
+
+      // insert all fields into a set
+      std::set<panzer::StrPureBasisPair,panzer::StrPureBasisComp> fieldNames;
+      fieldNames.insert(blockFields.begin(),blockFields.end());
+
+      // build string for modifiying vectors
+      std::vector<std::string> dimenStr(3);
+      dimenStr[0] = "X"; dimenStr[1] = "Y"; dimenStr[2] = "Z";
+
+      // add basis to DOF manager: block specific
+      std::set<panzer::StrPureBasisPair,panzer::StrPureBasisComp>::const_iterator fieldItr;
+      for (fieldItr=fieldNames.begin();fieldItr!=fieldNames.end();++fieldItr) {
+         Teuchos::RCP<const panzer::PureBasis> basis = fieldItr->second;
+         if(basis->getElementSpace()==panzer::PureBasis::HGRAD)
+            mesh->addSolutionField(fieldItr->first,pb->elementBlockID());
+         else if(basis->getElementSpace()==panzer::PureBasis::HCURL) {
+            for(int dim=0;dim<basis->dimension();++dim)
+               mesh->addCellField(fieldItr->first+dimenStr[dim],pb->elementBlockID());
+         }
+      }
+
+      mesh_factory->completeMeshConstruction(*mesh,MPI_COMM_WORLD);
+
+      mesh->setupExodusFile("output.exo");
+    }
+
+    // build worksets
+    //////////////////////////////////////////////////////////////
+
+    // build WorksetContainer
+    Teuchos::RCP<panzer_stk::WorksetFactory> wkstFactory
+       = Teuchos::rcp(new panzer_stk::WorksetFactory(mesh)); // build STK workset factory
+    Teuchos::RCP<panzer::WorksetContainer> wkstContainer             // attach it to a workset container (uses lazy evaluation)
+       = Teuchos::rcp(new panzer::WorksetContainer);
+    wkstContainer->setFactory(wkstFactory);
+    for(size_t i=0;i<physicsBlocks.size();i++)
+      wkstContainer->setNeeds(physicsBlocks[i]->elementBlockID(),physicsBlocks[i]->getWorksetNeeds());
+    wkstContainer->setWorksetSize(workset_size);
+
+    // build DOF Manager
+    /////////////////////////////////////////////////////////////
+
+    // build the connection manager
+    const Teuchos::RCP<panzer::ConnManager>
+      conn_manager = Teuchos::rcp(new panzer_stk::STKConnManager(mesh));
+
+    // build the state dof manager and LOF
+    RCP<panzer::GlobalIndexer> dofManager;
+    RCP<panzer::LinearObjFactory<panzer::Traits> > linObjFactory;
+    {
+      panzer::DOFManagerFactory globalIndexerFactory;
+      dofManager = globalIndexerFactory.buildGlobalIndexer(Teuchos::opaqueWrapper(MPI_COMM_WORLD),physicsBlocks,conn_manager);
+      linObjFactory = Teuchos::rcp(new panzer::TpetraLinearObjFactory<panzer::Traits,double,int,panzer::GlobalOrdinal>(comm,dofManager));
+    }
+
+    // build linear solver
+    /////////////////////////////////////////////////////////////
+
+    RCP<Thyra::LinearOpWithSolveFactoryBase<double> > lowsFactory
+        = panzer_stk::buildLOWSFactory(false, dofManager, conn_manager,
+                                               Teuchos::as<int>(mesh->getDimension()),
+                                               comm, lin_solver_pl,Teuchos::null);
+
+    // build and setup model evaluatorlinear solver
+    /////////////////////////////////////////////////////////////
+
+    std::vector<panzer::BC> bcs;
+    panzer::buildBCs(bcs,bcs_pl,globalData);
+
+    RCP<PME> physics = Teuchos::rcp(new PME(linObjFactory,lowsFactory,globalData,build_transient_support,0.0));
+    physics->setupModel(wkstContainer,physicsBlocks,bcs,
+                   *eqset_factory,
+                   bc_factory,
+                   cm_factory,
+                   cm_factory,
+                   closure_models_pl,
+                   user_data_pl,false,"");
+
+
+    // Get a vector, fill it with randomize
+    RCP<Thyra::VectorBase<double> > x = Thyra::createMember(physics->get_x_space());
+    {
+      RCP<Thyra::TpetraVector<double,int,panzer::GlobalOrdinal,panzer::TpetraNodeType> > t_x = rcp_dynamic_cast<Thyra::TpetraVector<double,int,panzer::GlobalOrdinal,panzer::TpetraNodeType> >(x);
+      t_x->getTpetraVector()->randomize();
+    }
+
+    // Get vectors for A*x
+    RCP<Thyra::VectorBase<double> > y_matfree = Thyra::createMember(physics->get_f_space());
+    RCP<Thyra::VectorBase<double> > y_mat = Thyra::createMember(physics->get_f_space());
+
+    int repeats = 100;
+
+    Thyra::ModelEvaluatorBase::InArgs<double> inArgs;
+    Thyra::ModelEvaluatorBase::OutArgs<double> outArgs;
+    inArgs = physics->createInArgs();
+    outArgs = physics->createOutArgs();
+    // apply matrix-free once
+    {
+      Teuchos::TimeMonitor tMmesh(*Teuchos::TimeMonitor::getNewTimer(std::string("apply matrix-free warmup")));
+      inArgs.set_x(x);
+      outArgs.set_f(y_matfree);
+      physics->evalModel(inArgs,outArgs);
+    }
+    // apply matrix-free a bunch of times
+    for (int rep = 0; rep < repeats; ++rep) {
+      Teuchos::TimeMonitor tMmesh(*Teuchos::TimeMonitor::getNewTimer(std::string("apply matrix-free")));
+      inArgs.set_x(x);
+      outArgs.set_f(y_matfree);
+      physics->evalModel(inArgs,outArgs);
+    }
+
+    {
+      RCP<Thyra::LinearOpBase<double> > A;
+      // build matrix
+      {
+        Teuchos::TimeMonitor tMmesh(*Teuchos::TimeMonitor::getNewTimer(std::string("build matrix")));
+        Thyra::ModelEvaluatorBase::InArgs<double> inArgsJac = physics->createInArgs();
+        Thyra::ModelEvaluatorBase::OutArgs<double> outArgsJac = physics->createOutArgs();
+        // set up the A
+        A = physics->create_W_op();
+        outArgs.set_W_op(A);
+        physics->evalModel(inArgsJac,outArgsJac);
+      }
+
+      // apply matrix a bunch of times
+      for (int rep = 0; rep < repeats; ++rep) {
+        Teuchos::TimeMonitor tMmesh(*Teuchos::TimeMonitor::getNewTimer(std::string("apply matrix")));
+        A->apply(Thyra::NOTRANS, *x, y_mat.ptr(), 1.0, 0.0);
+      }
+    }
+
+    {
+      const RCP<const Thyra::TpetraVector<double,int,panzer::GlobalOrdinal,panzer::TpetraNodeType> > tY_Matfree = rcp_dynamic_cast<const Thyra::TpetraVector<double,int,panzer::GlobalOrdinal,panzer::TpetraNodeType> >(y_matfree);
+      Tpetra::MatrixMarket::Writer<Tpetra::CrsMatrix<double,int,panzer::GlobalOrdinal,panzer::TpetraNodeType> >::writeDenseFile("y_matfree.mm", *tY_Matfree->getConstTpetraVector());
+    }
+
+    {
+      const RCP<const Thyra::TpetraVector<double,int,panzer::GlobalOrdinal,panzer::TpetraNodeType> > tY_Matfree = rcp_dynamic_cast<const Thyra::TpetraVector<double,int,panzer::GlobalOrdinal,panzer::TpetraNodeType> >(y_mat);
+      Tpetra::MatrixMarket::Writer<Tpetra::CrsMatrix<double,int,panzer::GlobalOrdinal,panzer::TpetraNodeType> >::writeDenseFile("y_mat.mm", *tY_Matfree->getConstTpetraVector());
+    }
+
+    y_matfree->update(-1.0, *y_mat);
+    std::cout << "Norm of difference: " << y_matfree->col(0)->norm_2() << std::endl;
+
+  }
+  catch (std::exception& e) {
+    *out << "*********** Caught Exception: Begin Error Report ***********" << std::endl;
+    *out << e.what() << std::endl;
+    *out << "************ Caught Exception: End Error Report ************" << std::endl;
+    status = -1;
+  }
+  catch (std::string& msg) {
+    *out << "*********** Caught Exception: Begin Error Report ***********" << std::endl;
+    *out << msg << std::endl;
+    *out << "************ Caught Exception: End Error Report ************" << std::endl;
+    status = -1;
+  }
+  catch (...) {
+    *out << "*********** Caught Exception: Begin Error Report ***********" << std::endl;
+    *out << "Caught UNKOWN exception" << std::endl;
+    *out << "************ Caught Exception: End Error Report ************" << std::endl;
+    status = -1;
+  }
+  stacked_timer->stop("Total");
+  Teuchos::StackedTimer::OutputOptions options;
+  options.output_fraction = options.output_histogram = options.output_minmax = true;
+  stacked_timer->report(*out, comm, options);
+
+  // Teuchos::TimeMonitor::summarize(*out,false,true,false);
+
+  if (status == 0)
+    *out << "panzer::MainDriver run completed." << std::endl;
+
+  return status;
+}
