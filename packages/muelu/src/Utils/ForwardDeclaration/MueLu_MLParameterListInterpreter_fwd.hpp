753d4bfecadefcdb9273d4454f16c762f00e00b8
cgcgcg <cgcgcg@users.noreply.github.com>
2023-11-08 04:24:59 +0000

    clang-format

--- packages/muelu/src/Utils/ForwardDeclaration/MueLu_MLParameterListInterpreter_fwd.hpp
+++ packages/muelu/src/Utils/ForwardDeclaration/MueLu_MLParameterListInterpreter_fwd.hpp
@@ -46,18 +46,13 @@
 #ifndef MUELU_MLPARAMETERLISTINTERPRETER_FWD_HPP
 #define MUELU_MLPARAMETERLISTINTERPRETER_FWD_HPP
 
-
-
-
 namespace MueLu {
-  template <class Scalar, class LocalOrdinal, class GlobalOrdinal, class Node>
-  class MLParameterListInterpreter;
+template <class Scalar, class LocalOrdinal, class GlobalOrdinal, class Node>
+class MLParameterListInterpreter;
 }
 
 #ifndef MUELU_MLPARAMETERLISTINTERPRETER_SHORT
 #define MUELU_MLPARAMETERLISTINTERPRETER_SHORT
 #endif
 
-
-
-#endif // MUELU_MLPARAMETERLISTINTERPRETER_FWD_HPP
+#endif  // MUELU_MLPARAMETERLISTINTERPRETER_FWD_HPP
