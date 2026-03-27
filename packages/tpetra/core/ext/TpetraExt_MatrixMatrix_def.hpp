5f1f46b5c374d0647958e62e7cb0694696d20ac0
Christian Glusa <caglusa@sandia.gov>
2026-03-27 12:50:11 -0600

    Tpetra: Remove global collective in import_and_extract_views. Pass PL to map constructor
    Signed-off-by: Christian Glusa <caglusa@sandia.gov>

--- packages/tpetra/core/ext/TpetraExt_MatrixMatrix_def.hpp
+++ packages/tpetra/core/ext/TpetraExt_MatrixMatrix_def.hpp
@@ -3069,7 +3069,7 @@ void import_and_extract_views(
         remoteRows[i] = targetMap->getGlobalElement(remoteLIDs[i]);
 
       remoteRowMap = rcp(new map_type(Teuchos::OrdinalTraits<global_size_t>::invalid(), remoteRows(),
-                                      rowMap->getIndexBase(), rowMap->getComm()));
+                                      rowMap->getIndexBase(), rowMap->getComm(), params));
       mode         = 1;
 
     } else if (prototypeImporter.is_null()) {
@@ -3088,7 +3088,7 @@ void import_and_extract_views(
       }
       remoteRows.resize(numRemote);
       remoteRowMap = rcp(new map_type(Teuchos::OrdinalTraits<global_size_t>::invalid(), remoteRows(),
-                                      rowMap->getIndexBase(), rowMap->getComm()));
+                                      rowMap->getIndexBase(), rowMap->getComm(), params));
       mode         = 2;
 
     } else {
@@ -3104,16 +3104,9 @@ void import_and_extract_views(
     }
 
     //
-    // Now we will import the needed remote rows of A, if the global maximum
-    // value of numRemote is greater than 0.
+    // Now we will import the needed remote rows of A, if the remoteRowMap has entries
     //
-    MM = Teuchos::null;
-    MM = rcp(new Tpetra::Details::ProfilingRegion("TpetraExt: MMM: I&X Collective-0"));
-
-    global_size_t globalMaxNumRemote = 0;
-    Teuchos::reduceAll(*(rowMap->getComm()), Teuchos::REDUCE_MAX, (global_size_t)numRemote, Teuchos::outArg(globalMaxNumRemote));
-
-    if (globalMaxNumRemote > 0) {
+    if (!remoteRowMap.is_null() && (remoteRowMap->getGlobalNumElements() > 0)) {
       MM = Teuchos::null;
       MM = rcp(new Tpetra::Details::ProfilingRegion("TpetraExt: MMM: I&X Import-2"));
 
@@ -3280,14 +3273,11 @@ void import_and_extract_views(
     return;
   }
 
-  // Now we will import the needed remote rows of M, if the global maximum
-  // value of numRemote is greater than 0.
-  global_size_t globalMaxNumRemote = 0;
-  Teuchos::reduceAll(*(rowMap->getComm()), Teuchos::REDUCE_MAX, (global_size_t)numRemote, Teuchos::outArg(globalMaxNumRemote));
+  // Now we will import the needed remote rows of M, if the remoteRowMap has entries
 
   RCP<const import_type> importer;
 
-  if (globalMaxNumRemote > 0) {
+  if (!remoteRowMap.is_null() && (remoteRowMap->getGlobalNumElements() > 0)) {
     // Create an importer with target-map remoteRowMap and source-map rowMap.
     if (mode == 1)
       importer = prototypeImporter->createRemoteOnlyImport(remoteRowMap);
