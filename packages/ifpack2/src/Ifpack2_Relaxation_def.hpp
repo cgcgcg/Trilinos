d27994a2304d1a4680545a690a15e24c64f4edf7
Christian Glusa <caglusa@sandia.gov>
2026-04-11 13:49:36 -0600

    Ifpack2 Chebychev&Relaxation: Do not assume global constants
    Signed-off-by: Christian Glusa <caglusa@sandia.gov>

--- packages/ifpack2/src/Ifpack2_Relaxation_def.hpp
+++ packages/ifpack2/src/Ifpack2_Relaxation_def.hpp
@@ -1379,10 +1379,13 @@ void Relaxation<MatrixType>::
 
   // Floating-point operations due to the damping factor, per matrix
   // row, per direction, per columm of output.
-  const double numGlobalNonzeros = as<double>(A_->getGlobalNumEntries());
-  const double dampingFlops      = (DampingFactor_ == STS::one()) ? 0.0 : 1.0;
-  ApplyFlops_ += as<double>(NumSweeps_ - startSweep) * numVectors *
-                 (2.0 * numGlobalNonzeros + dampingFlops);
+  auto crsMat = Details::getCrsMatrix(A_);
+  if (!crsMat.is_null() && crsMat->haveGlobalConstants()) {
+    const double numGlobalNonzeros = as<double>(A_->getGlobalNumEntries());
+    const double dampingFlops      = (DampingFactor_ == STS::one()) ? 0.0 : 1.0;
+    ApplyFlops_ += as<double>(NumSweeps_ - startSweep) * numVectors *
+                   (2.0 * numGlobalNonzeros + dampingFlops);
+  }
 }
 
 template <class MatrixType>
@@ -1446,10 +1449,13 @@ void Relaxation<MatrixType>::
 
   // Floating-point operations due to the damping factor, per matrix
   // row, per direction, per columm of output.
-  const double numGlobalNonzeros = as<double>(A_->getGlobalNumEntries());
-  const double dampingFlops      = (DampingFactor_ == STS::one()) ? 0.0 : 1.0;
-  ApplyFlops_ += as<double>(NumSweeps_ - startSweep) * numVectors *
-                 (2.0 * numGlobalRows + 2.0 * numGlobalNonzeros + dampingFlops);
+  auto crsMat = Details::getCrsMatrix(A_);
+  if (!crsMat.is_null() && crsMat->haveGlobalConstants()) {
+    const double numGlobalNonzeros = as<double>(A_->getGlobalNumEntries());
+    const double dampingFlops      = (DampingFactor_ == STS::one()) ? 0.0 : 1.0;
+    ApplyFlops_ += as<double>(NumSweeps_ - startSweep) * numVectors *
+                   (2.0 * numGlobalRows + 2.0 * numGlobalNonzeros + dampingFlops);
+  }
 }
 
 template <class MatrixType>
@@ -1597,12 +1603,15 @@ void Relaxation<MatrixType>::
   }
 
   // See flop count discussion in implementation of ApplyInverseGS_CrsMatrix().
-  const double dampingFlops      = (DampingFactor_ == STS::one()) ? 0.0 : 1.0;
-  const double numVectors        = as<double>(X.getNumVectors());
-  const double numGlobalRows     = as<double>(A_->getGlobalNumRows());
-  const double numGlobalNonzeros = as<double>(A_->getGlobalNumEntries());
-  ApplyFlops_ += 2.0 * NumSweeps_ * numVectors *
-                 (2.0 * numGlobalRows + 2.0 * numGlobalNonzeros + dampingFlops);
+  auto crsMat = Details::getCrsMatrix(A_);
+  if (!crsMat.is_null() && crsMat->haveGlobalConstants()) {
+    const double dampingFlops      = (DampingFactor_ == STS::one()) ? 0.0 : 1.0;
+    const double numVectors        = as<double>(X.getNumVectors());
+    const double numGlobalRows     = as<double>(A_->getGlobalNumRows());
+    const double numGlobalNonzeros = as<double>(A_->getGlobalNumEntries());
+    ApplyFlops_ += 2.0 * NumSweeps_ * numVectors *
+                   (2.0 * numGlobalRows + 2.0 * numGlobalNonzeros + dampingFlops);
+  }
 }
 
 template <class MatrixType>
@@ -1745,12 +1754,15 @@ void Relaxation<MatrixType>::
 
   // Floating-point operations due to the damping factor, per matrix
   // row, per direction, per columm of output.
-  const double dampingFlops      = (DampingFactor_ == STS::one()) ? 0.0 : 1.0;
-  const double numVectors        = X.getNumVectors();
-  const double numGlobalRows     = A_->getGlobalNumRows();
-  const double numGlobalNonzeros = A_->getGlobalNumEntries();
-  ApplyFlops_ += NumSweeps_ * numVectors *
-                 (2.0 * numGlobalRows + 2.0 * numGlobalNonzeros + dampingFlops);
+  auto crsMat = Details::getCrsMatrix(A_);
+  if (!crsMat.is_null() && crsMat->haveGlobalConstants()) {
+    const double dampingFlops      = (DampingFactor_ == STS::one()) ? 0.0 : 1.0;
+    const double numVectors        = X.getNumVectors();
+    const double numGlobalRows     = A_->getGlobalNumRows();
+    const double numGlobalNonzeros = A_->getGlobalNumEntries();
+    ApplyFlops_ += NumSweeps_ * numVectors *
+                   (2.0 * numGlobalRows + 2.0 * numGlobalNonzeros + dampingFlops);
+  }
 }
 
 template <class MatrixType>
@@ -2078,15 +2090,17 @@ void Relaxation<MatrixType>::
     }
   }
 
-  const double dampingFlops      = (DampingFactor_ == STS::one()) ? 0.0 : 1.0;
-  const double numVectors        = as<double>(X.getNumVectors());
-  const double numGlobalRows     = as<double>(A_->getGlobalNumRows());
-  const double numGlobalNonzeros = as<double>(A_->getGlobalNumEntries());
-  double ApplyFlops              = NumSweeps_ * numVectors *
-                      (2.0 * numGlobalRows + 2.0 * numGlobalNonzeros + dampingFlops);
-  if (direction == Tpetra::Symmetric)
-    ApplyFlops *= 2.0;
-  ApplyFlops_ += ApplyFlops;
+  if (crsMat->haveGlobalConstants()) {
+    const double dampingFlops      = (DampingFactor_ == STS::one()) ? 0.0 : 1.0;
+    const double numVectors        = as<double>(X.getNumVectors());
+    const double numGlobalRows     = as<double>(A_->getGlobalNumRows());
+    const double numGlobalNonzeros = as<double>(A_->getGlobalNumEntries());
+    double ApplyFlops              = NumSweeps_ * numVectors *
+                        (2.0 * numGlobalRows + 2.0 * numGlobalNonzeros + dampingFlops);
+    if (direction == Tpetra::Symmetric)
+      ApplyFlops *= 2.0;
+    ApplyFlops_ += ApplyFlops;
+  }
 }
 
 template <class MatrixType>
@@ -2181,8 +2195,11 @@ std::string Relaxation<MatrixType>::description() const {
     os << "Matrix: null";
   } else {
     os << "Global matrix dimensions: ["
-       << A_->getGlobalNumRows() << ", " << A_->getGlobalNumCols() << "]"
-       << ", Global nnz: " << A_->getGlobalNumEntries();
+       << A_->getGlobalNumRows() << ", " << A_->getGlobalNumCols() << "]";
+    auto crsMat = Details::getCrsMatrix(A_);
+    if (!crsMat.is_null() && crsMat->haveGlobalConstants()) {
+      os << ", Global nnz: " << A_->getGlobalNumEntries();
+    }
   }
 
   os << "}";
