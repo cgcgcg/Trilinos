6423d74bccd99f99e26a04bd423aebd37daddad4
Christian Glusa <caglusa@sandia.gov>
2026-04-11 14:05:41 -0600

    MueLu: Compute global constants only when needed
    Signed-off-by: Christian Glusa <caglusa@sandia.gov>

--- packages/muelu/src/Misc/MueLu_RAPFactory_def.hpp
+++ packages/muelu/src/Misc/MueLu_RAPFactory_def.hpp
@@ -102,6 +102,8 @@ void RAPFactory<Scalar, LocalOrdinal, GlobalOrdinal, Node>::Build(Level& fineLev
       return;
     }
 
+    const bool computeGlobalConstantsForAc = IsPrint(Statistics0);
+
     bool isGPU = Node::is_gpu;
 
     if (pL.get<bool>("rap: triple product") == false || isGPU) {
@@ -154,7 +156,7 @@ void RAPFactory<Scalar, LocalOrdinal, GlobalOrdinal, Node>::Build(Level& fineLev
 
       // We *always* need global constants for the RAP, but not for the temps
       RAPparams->set("compute global constants: temporaries", RAPparams->get("compute global constants: temporaries", false));
-      RAPparams->set("compute global constants", true);
+      RAPparams->set("compute global constants", computeGlobalConstantsForAc);
 
       // Allow optimization of storage.
       // This is necessary for new faster Epetra MM kernels.
@@ -237,7 +239,7 @@ void RAPFactory<Scalar, LocalOrdinal, GlobalOrdinal, Node>::Build(Level& fineLev
 
       // We *always* need global constants for the RAP, but not for the temps
       RAPparams->set("compute global constants: temporaries", RAPparams->get("compute global constants: temporaries", false));
-      RAPparams->set("compute global constants", true);
+      RAPparams->set("compute global constants", computeGlobalConstantsForAc);
 
       if (pL.get<bool>("transpose: use implicit") == true) {
         Ac = MatrixFactory::Build(P->getDomainMap(), Teuchos::as<LO>(0));
